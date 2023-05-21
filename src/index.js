77d04d7ba54a5a1194ca01b40a89e0e2255b8a7c
radevden <radevden@gmail.com>
2023-05-21 13:12:13 +0200

    js not working

---
+++ src/index.js
@@ -0,0 +1,5 @@
+let portoElement = document.querySelector("#porto");
+let portoDateElement = portoElement.querySelector(".date");
+let portoTimeElement = portoElement.querySelector(".time");
+portoDateElement.innerHTML = "Saturday";
+portoTimeElement.innerHTML = "19:20";
