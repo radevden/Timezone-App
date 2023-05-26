03127d7e07ff9ff7c9ffedf3f4825e4b0a0656db
radevden <radevden@gmail.com>
2023-05-26 21:07:12 +0300

    added JS time+date

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-let portoElement = document.querySelector("#porto");
-let portoDateElement = portoElement.querySelector(".date");
-let portoTimeElement = portoElement.querySelector(".time");
-portoDateElement.innerHTML = "Saturday";
-portoTimeElement.innerHTML = "19:20";
+function updateTime() {
+  //athens
+  let athensElement = document.querySelector("#athens");
+  let athensDateElement = document.querySelector(".date-athens");
+  let athensTimeElement = document.querySelector(".time-athens");
+
+  let athensTimeDate = moment().tz("Europe/Athens");
+  athensDateElement.innerHTML = athensTimeDate.format("dddd, MMMM Do YYYY");
+  athensTimeElement.innerHTML = athensTimeDate.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //london
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = document.querySelector(".date-london");
+  let londonTimeElement = document.querySelector(".time-london");
+
+  let londonTimeDate = moment().tz("Europe/London");
+  londonDateElement.innerHTML = londonTimeDate.format("dddd, MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTimeDate.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+setInterval(updateTime, 1000);
